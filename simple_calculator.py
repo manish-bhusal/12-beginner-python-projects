ebdf5fa4db47a0eb17b82ca81ac22c493e955f06
manish-bhusal <manishbhusal6@gmail.com>
2023-01-18 16:27:32 +0545

    Create calculator function for performing basic mathematical operations

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,23 @@
+def calculator(first_number, operator, second_number):
+    """
+    This function takes in 3 arguments: first_number, operator and second_number
+    and performs the operation based on the operator and returns the result.
+    """
+    # Perform the operation based on the operator
+    if operator == "+":
+        result = first_number + second_number
+    elif operator == "-":
+        result = first_number - second_number
+    elif operator == "*":
+        result = first_number * second_number
+    elif operator == "/":
+        result = first_number / second_number
+    else:
+        return "Invalid operator"
+
+    return result
+
+
 # Get the first number from user
 first_number = float(input("Enter the first number: "))
 
@@ -7,19 +27,8 @@ operator = input("Enter the operator (+, -, *, /): ")
 # Get the second number from user
 second_number = float(input("Enter the second number: "))
 
-# Perform the operation based on the operator
-if operator == "+":
-    result = first_number + second_number
-elif operator == "-":
-    result = first_number - second_number
-elif operator == "*":
-    result = first_number * second_number
-elif operator == "/":
-    result = first_number / second_number
-else:
-    print("Invalid operator")
-    result = None
+# Call the calculator function and store the result
+result = calculator(first_number, operator, second_number)
 
 # Print the result
-if result is not None:
-    print("Result:", result)
+print("Result:", result)
